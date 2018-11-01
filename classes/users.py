7734ddaa22c6c6def9dfa9747e03ad0ace7ccb78
Christian <c.lamothe25@gmail.com>
2018-11-01 16:22:23 -0400

    More practice with classes

---
+++ classes/users.py
@@ -0,0 +1,22 @@
+class Users():
+
+    def __init__(self, first_name, last_name, id, city):
+        self.first = first_name
+        self.last = last_name
+        self.id = id
+        self.city = city
+
+    def describe_user(self):
+        print(f"{self.first.title()} {self.last.title()} lives in {self.city.title()}.")
+        print(f"{self.last.title()} ID: {self.id}")
+
+    def greet_user(self):
+        print(f"Hello {self.id}, welcome to the Terrordome.")
+
+user_1 = Users('Chris', 'lamothe', 'de34jks', 'providence')
+user_2 = Users('mr', 'robot', '88rd3sw3', 'new york')
+
+user_2.describe_user()
+user_2.greet_user()
+
+    
